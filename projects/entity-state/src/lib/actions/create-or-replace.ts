09eec3cdf74199a6291656bb6b89a990c5066c49
JanMalch <janmalch@gmail.com>
2018-12-23 17:31:36 +0100

    docs: updated Add and CreateOrReplace action generators

--- projects/entity-state/src/lib/actions/create-or-replace.ts
+++ projects/entity-state/src/lib/actions/create-or-replace.ts
@@ -5,13 +5,14 @@ import { Payload } from './type-alias';
 
 export type EntityCreateOrReplaceAction<T> = Payload<T | T[]>;
 
-// TODO: behaviour? Should add also replace if it exists? Separate CreateOrReplace?
 export class CreateOrReplace<T> {
   /**
    * Generates an action that will add the given entities to the state.
    * If an entity with the ID already exists, it will be overridden.
+   * In all cases it will overwrite the ID value in the entity with the calculated ID.
    * @param target The targeted state class
    * @param payload An entity or an array of entities to be added
+   * @see Add#constructor
    */
   constructor(target: Type<EntityState<T>>, payload: T | T[]) {
     return generateActionObject('createOrReplace', target, payload);
