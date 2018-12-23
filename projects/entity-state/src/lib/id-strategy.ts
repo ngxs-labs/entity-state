95e938a5f03058b68d88668facb0433e13999012
JanMalch <janmalch@gmail.com>
2018-12-23 18:05:38 +0100

    docs: added documentation for base class

--- projects/entity-state/src/lib/id-strategy.ts
+++ projects/entity-state/src/lib/id-strategy.ts
@@ -5,17 +5,45 @@ export namespace IdStrategy {
   export abstract class IdGenerator<T> {
     protected constructor(protected readonly idKey: keyof T) {}
 
+    /**
+     * Generates a completely new ID.
+     * The IdGenerator's implementation has to ensure that the generated ID does not exist in the current state.
+     * It can throw an UnableToGenerateIdError if it's unable to do so.
+     * @param entity The entity to generate an ID for
+     * @param state The current state
+     * @see getPresentIdOrGenerate
+     * @see UnableToGenerateIdError
+     */
     abstract generateId(entity: Partial<T>, state: EntityStateModel<any>): string;
 
+    /**
+     * Checks if the given id is in the state's ID array
+     * @param id the ID to check
+     * @param state the current state
+     */
     isIdInState(id: string, state: EntityStateModel<any>): boolean {
       return state.ids.includes(id);
     }
 
+    /**
+     * This function tries to get the present ID of the given entity with #getIdOf.
+     * If it's undefined the #generateId function will be used.
+     * @param entity The entity to get the ID from
+     * @param state The current state
+     * @see getIdOf
+     * @see generateId
+     */
     getPresentIdOrGenerate(entity: Partial<T>, state: EntityStateModel<any>): string {
       const presentId = this.getIdOf(entity);
       return presentId === undefined ? this.generateId(entity, state) : presentId;
     }
 
+    /**
+     * A wrapper for #getIdOf. If the function returns undefined an error will be thrown.
+     * @param entity The entity to get the ID from
+     * @see getIdOf
+     * @see InvalidIdOfError
+     */
     mustGetIdOf(entity: any): string {
       const id = this.getIdOf(entity);
       if (id === undefined) {
@@ -24,6 +52,10 @@ export namespace IdStrategy {
       return id;
     }
 
+    /**
+     * Returns the ID for the given entity. Can return undefined.
+     * @param entity The entity to get the ID from
+     */
     getIdOf(entity: any): string | undefined {
       return entity[this.idKey];
     }
