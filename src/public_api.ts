46ed807ea785cc60c44688e9939a3ad58ebf6201
JanMalch <janmalch@gmail.com>
2019-01-08 20:04:41 +0100

    fix: removed internal meta key from public API

--- src/public_api.ts
+++ src/public_api.ts
@@ -2,4 +2,3 @@ export * from './lib/actions';
 export * from './lib/entity-state';
 export * from './lib/errors';
 export * from './lib/id-strategy';
-export { NGXS_META_KEY } from './lib/internal';
