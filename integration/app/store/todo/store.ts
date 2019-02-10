5edca5f4f648ecb136945423aa96aa28991dd449
JanMalch <janmalch@gmail.com>
2019-02-10 10:41:58 +0100

    refactor: enforce entity type

--- integration/app/store/todo/store.ts
+++ integration/app/store/todo/store.ts
@@ -20,8 +20,4 @@ export class TodoState extends EntityState<ToDo> {
   constructor() {
     super(TodoState, 'title', IdStrategy.EntityIdGenerator);
   }
-
-  onUpdate(current: Readonly<ToDo>, updated: Partial<ToDo>): ToDo {
-    return { ...current, ...updated };
-  }
 }
