28c7994f75c6d97ed64595df737907e3ae275ce3
Jan Malchert <25508038+JanMalch@users.noreply.github.com>
2020-02-24 12:51:23 +0100

    refactor: add RemoveAll and UpdateAll actions (#197)
    * refactor: add RemoveAll and UpdateAll actions
    
    BREAKING CHANGE: using null as a selector will cause Remove and Update to throw an error
    
    * test: remove specific expected error type

--- src/lib/actions/remove.ts
+++ src/lib/actions/remove.ts
@@ -8,12 +8,22 @@ export type EntityRemoveAction<T> = Payload<EntitySelector<T>>;
 export class Remove<T> {
   /**
    * Generates an action that will remove the given entities from the state.
-   * Put null if all entities should be removed.
    * @param target The targeted state class
    * @param payload An EntitySelector payload
    * @see EntitySelector
+   * @see RemoveAll
    */
   constructor(target: Type<EntityState<T>>, payload: EntitySelector<T>) {
     return generateActionObject(EntityActionType.Remove, target, payload);
+  }
+}
+
+export class RemoveAll {
+  /**
+   * Generates an action that will remove all entities from the state.
+   * @param target The targeted state class
+   */
+  constructor(target: Type<EntityState<any>>) {
+    return generateActionObject(EntityActionType.RemoveAll, target);
   }
 }
